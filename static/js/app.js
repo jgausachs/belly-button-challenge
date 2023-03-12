2b8814519f7af885d2d94127d5807db3b7f6c410
jgausachs <jgausachs@yahoo.com>
2023-03-12 23:07:09 +1100

    Final commit. Partial solution to challenge

--- static/js/app.js
+++ static/js/app.js
@@ -9,46 +9,57 @@ let firstSample = {};
 let samplevalues = [];
 let otuLabels = [];
 
+
+
 d3.json(url).then(function (data) {
-    firstSample = data.samples[0];
+    let samples = data.samples;
+    firstSample = samples[0];
 
-    // ++++++++++++++++++++++++++++++++
-    // 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
-    // Sort firstSample by sample values
-    let sortedSample = firstSample.sample_values.map(function (value, index) {
-        return {
-            sample_value: value,
-            otu_id: firstSample.otu_ids[index],
-            otu_labels: firstSample.otu_labels[index]
+    function chartselection(clickId) {
+        console.log(clickId);
+
+        // 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+        // Sort firstSample by sample values
+
+        let resultClick = samples.filter(sample => sample.id == clickId);
+        let result = resultClick[0];
+        console.log(result);
+
+        let sortedSample = firstSample.sample_values.map(function (value, index) {
+            return {
+                sample_value: value,
+                otu_id: firstSample.otu_ids[index],
+                otu_labels: firstSample.otu_labels[index]
+            };
+        }).sort((a, b) => b.sample_values - a.sample_values).slice(0, 10).reverse();
+
+        console.log(sortedSample);
+        let sampleValues = firstSample.sample_values.slice(0, 10).reverse();
+
+        // Trace for the sample data
+        let trace1 = {
+            x: sampleValues,
+            y: sortedSample.map(s => `ID: ${s.otu_id}  `),
+            text: sortedSample.map(s => s.otu_labels),
+            type: "bar",
+            orientation: "h"
         };
-    }).sort((a, b) => b.sample_values - a.sample_values).slice(0, 10).reverse();
 
-    console.log(sortedSample);
-    let sampleValues = firstSample.sample_values.slice(0, 10).reverse();
+        // Data trace1 array
+        let numval1 = [trace1];
 
-    // Trace for the sample data
-    let trace1 = {
-        x: sampleValues,
-        y: sortedSample.map(s => `ID: ${s.otu_id}  `),
-        text: sortedSample.map(s => s.otu_labels),
-        type: "bar",
-        orientation: "h"
-    };
+        // Apply the group bar mode to the layout
+        let layout1 = {
+            hovermode: "closest",
+            title: "Top 10 Operational Taxonomic Units for Test Subject"
+        };
+        console.log(numval1);
 
-    // Data trace1 array
-    let numval1 = [trace1];
+        // Render the plot to the div tag with id "plot"
+        Plotly.newPlot("bar", numval1, layout1);
 
-    // Apply the group bar mode to the layout
-    let layout1 = {
-        hovermode: "closest",
-        title: "Top 10 Operational Taxonomic Units"
-    };
-    console.log(numval1);
+    }
 
-    // Render the plot to the div tag with id "plot"
-    Plotly.newPlot("bar", numval1, layout1);
-
-    // ++++++++++++++++++++++++++++++++
     // 3. Create a bubble chart that displays each sample.
     // Trace for the sample data
     let trace2 = {
@@ -68,7 +79,7 @@ d3.json(url).then(function (data) {
     // Apply the group bar mode to the layout
     let layout2 = {
         hovermode: "closest",
-        title: "Sample Values by Operational Taxonomic Unit ID",
+        title: "Distribution of Operational Taxonomic Units for Test Subject",
         xaxis: { title: "OTU ID" }
     };
     console.log(numval2);
@@ -76,7 +87,6 @@ d3.json(url).then(function (data) {
     // Render the plot to the div tag with id "plot"
     Plotly.newPlot("bubble", numval2, layout2);
 
-    // ++++++++++++++++++++++++++++++++
     // 4. Display the sample metadata
     // Get the metadata for the first sample
     let sampleMetadata = data.metadata[0];
@@ -84,12 +94,10 @@ d3.json(url).then(function (data) {
     // Fetch data for `#sample-metadata` corresponding to the first sample
     let display = d3.select("#sample-metadata");
 
+    // 5. Display each key-value pair from the metadata JSON object somewhere on the page
     // Add data (key-value pair)
-    Object.entries(sampleMetadata).
-        forEach(([key, value]) => {
-            display.append("h6").text(`${key}: ${value}`);
-        }
-        );
+    Object.entries(sampleMetadata).forEach(([key, value]) => { display.append("h6").text(`${key}: ${value}`); });
+
+    chartselection(940);
 
 });
-
